e782aa80a275c9e6c6ead67a6e5df04c8e8988d5
Oluwamayowa <118119872+Ademayo959@users.noreply.github.com>
2025-09-29 13:42:08 -0700

    Add files via upload

---
+++ Ceevì/log-in.js
@@ -0,0 +1,13 @@
+const userPassword = document.querySelector('.input-password');
+const eyeBtn = document.querySelector('.eye');
+const eyeImg = document.querySelector('.eye-img');
+
+eyeBtn.addEventListener('click', ()=>{
+    if (userPassword.type === 'password' && eyeImg.getAttribute('src', 'img/eye.png') ){
+        userPassword.type = 'text'
+        eyeImg.setAttribute('src', 'img/eye-off.png')
+    } else {
+        userPassword.type = 'password'
+        eyeImg.setAttribute('src', 'img/eye.png')
+    };
+});
